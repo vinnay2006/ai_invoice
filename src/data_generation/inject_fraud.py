68e11aee98e8514da9a242cf577b462d035208bd
vinnay2006 <vinaykumar1973.vkb@gmail.com>
2026-06-06 18:11:56 +0530

    Added inflated amount fraud pattern

--- src/data_generation/inject_fraud.py
+++ src/data_generation/inject_fraud.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import random
-
+df = pd.read_csv("data/raw/invoices_with_fraud.csv")
 df = pd.read_csv("data/raw/invoices.csv")
 fraud_count = int(len(df) * 0.05)
 duplicate_rows = df.sample(
@@ -14,6 +14,27 @@ df = pd.concat(
     ignore_index=True
 )
 print(df["fraud"].value_counts())
+df.to_csv(
+    "data/raw/invoices_with_fraud.csv",
+    index=False
+)
+#adding inflation in record and setting as fraud
+clean_df = df[df["fraud"] == 0]
+inflated_rows = clean_df.sample(
+    n=10,
+    random_state=1
+).copy()
+inflated_rows["total_amount"] = (
+    inflated_rows["total_amount"] * 3
+)
+inflated_rows["tax_amount"] = (
+    inflated_rows["total_amount"] * 0.18
+)
+inflated_rows["fraud"] = 1
+df = pd.concat(
+    [df, inflated_rows],
+    ignore_index=True
+)
 df.to_csv(
     "data/raw/invoices_with_fraud.csv",
     index=False
