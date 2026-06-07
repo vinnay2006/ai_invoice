05de4b421068e2e043323d27ccbe97f3fc53e442
vinnay2006 <vinaykumar1973.vkb@gmail.com>
2026-06-07 23:48:31 +0530

    Trained fraud detection models and evaluated performance

--- models/train_model.py
+++ models/train_model.py
@@ -50,3 +50,71 @@ X_train_smote, y_train_smote = (
 print(y_train_smote.value_counts())
 print(X_train_smote.shape)
 print(y_train_smote.shape)
+#training isolation forest -unsupervised learning 
+from sklearn.ensemble import IsolationForest
+
+iso_forest = IsolationForest(
+    contamination=0.05,
+    random_state=42
+)
+
+iso_forest.fit(X_train)
+iso_scores = iso_forest.decision_function(X_test)
+print(iso_scores[:10])
+
+#training the supersed model of learning XGBoost
+from xgboost import XGBClassifier
+xgb_model = XGBClassifier(
+    random_state=42,
+    n_estimators=200,
+    max_depth=5,
+    learning_rate=0.05,
+    eval_metric="logloss"
+)
+xgb_model.fit(
+    X_train_smote,
+    y_train_smote
+)
+y_pred = xgb_model.predict(X_test)
+
+y_prob = xgb_model.predict_proba(X_test)[:,1]
+
+from sklearn.metrics import (
+    precision_score,
+    recall_score,
+    f1_score,
+    roc_auc_score,
+    classification_report
+)
+print("Precision:", precision_score(y_test, y_pred))
+print("Recall:", recall_score(y_test, y_pred))
+print("F1:", f1_score(y_test, y_pred))
+print("ROC AUC:", roc_auc_score(y_test, y_prob))
+
+print(classification_report(y_test, y_pred))
+
+#tell which feature flagged the invoice
+importance = pd.DataFrame({
+    "feature": X_train.columns,
+    "importance": xgb_model.feature_importances_
+})
+
+importance = importance.sort_values(
+    by="importance",
+    ascending=False
+)
+
+print(importance)
+import joblib
+
+joblib.dump(
+    iso_forest,
+    "models/isolation_forest.pkl"
+)
+
+joblib.dump(
+    xgb_model,
+    "models/xgboost_model.pkl"
+)
+
+print("Models saved successfully")
