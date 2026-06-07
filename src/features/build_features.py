cc3b38d942e86ceb93c552237c254715e58a8bed
vinnay2006 <vinaykumar1973.vkb@gmail.com>
2026-06-07 20:17:08 +0530

    made initial feature engineering pipeline

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -27,3 +27,48 @@ df["days_to_due"] = (
     df["due_date"] - df["invoice_date"]
 ).dt.days
 print(df["days_to_due"].describe())
+#payment done on weekends which are usually off days
+df["weekend_submission"] = (
+    df["invoice_date"]
+    .dt.dayofweek
+    .isin([5, 6])
+    .astype(int)
+)
+print(
+    df["weekend_submission"]
+    .value_counts()
+)
+
+#payments of amount just less then threshold to avoid extra scrutiny 
+APPROVAL_LIMIT = 100000
+
+df["amount_threshold_proximity"] = (
+    APPROVAL_LIMIT
+    - df["total_amount"]
+).abs()
+
+print(
+    df["amount_threshold_proximity"]
+    .sort_values()
+    .head(20)
+)
+
+#checking the records for the consitency in the value unit_price*qty==total_amount
+expected_total = (
+    df["unit_price"]
+    * df["quantity"]
+)
+
+df["line_item_price_consistency"] = (
+    expected_total
+    == df["total_amount"]
+).astype(int)
+print(
+    df["line_item_price_consistency"]
+    .value_counts()
+)
+
+df.to_csv(
+    "data/processed/features_v1.csv",
+    index=False
+)
